3334ac29f6fa05a1c43a9af19d68dd8f59198791
oneureka <65535758+oneureka@users.noreply.github.com>
2026-07-31 18:15:20 +0800

    add IRB-style line-number prompts and fix readline history for continuation

--- lib/taoism/console.rb
+++ lib/taoism/console.rb
@@ -1,14 +1,18 @@
 module Taoism
   class Console
+    PS1 = 'taoism:%03d> '
+    PS2 = 'taoism:%03d* '
+
     def initialize
       @evaluator = Evaluator.new
+      @line_no = 1
     end
 
     def run
       require 'readline'
 
       loop do
-        input = prompt('taoism> ')
+        input = read_input
         break unless input
 
         break if input.strip == 'exit'
@@ -16,17 +20,30 @@ module Taoism
       end
     end
 
-    def prompt(prefix = '> ')
-      input = Readline.readline(prefix, true)
+    def read_input
+      ps1 = PS1 % @line_no
+      ps2 = PS2 % @line_no
+
+      input = Readline.readline(ps1, true)
       return if input.nil?
       input = input.chomp
 
+      had_continuation = false
+
       while unclosed?(input)
-        line = Readline.readline('..... ', true)
+        had_continuation = true
+        line = Readline.readline(ps2, false)
+
         break unless line
         input << line.chomp
       end
 
+      if had_continuation
+        Readline::HISTORY.pop
+        Readline::HISTORY << input
+      end
+
+      @line_no += 1
       input
     end
 
@@ -38,7 +55,7 @@ module Taoism
 
       if parser.errors.empty?
         result = @evaluator.evaluate(ast)
-        puts display(result)
+        puts render(result)
       else
         parser.errors.each do |err|
           $stderr.puts "Error: #{err}"
@@ -48,23 +65,19 @@ module Taoism
       $stderr.puts "Error: #{err.message}"
     end
 
-    def display(value)
+    def render(value)
       case value
       when nil    then 'None'
       when true   then 'True'
       when false  then 'False'
       when String then %Q("#{value}")
-      when Array  then "[#{value.map { display(_1) }.join(', ')}]"
+      when Array  then "[#{value.map { render(_1) }.join(', ')}]"
       else value.to_s
       end
     end
 
     def unclosed?(input)
-      nesting('{', '}', input) > 0
-    end
-
-    def nesting(open, close, str)
-      str.count(open) - str.count(close)
+      input.count('{') > input.count('}')
     end
   end
 end
